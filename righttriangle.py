d303008c332306a149700654f344478b97152d20
varman897 <varmansivarooban2015@gmail.com>
2026-03-17 19:42:51 -0700

    right triangular

---
+++ righttriangle.py
@@ -0,0 +1,9 @@
+print("Half pyrimid pattern of stars('):")
+n=int(input("Enter the number of rows:"))
+
+for i in range(n):
+
+    for j in range(i+1):
+        print("*", end="")
+    print()
+    
